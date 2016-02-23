f201ffc651430f10acc989582e053b86512d8932
Jian-Long Huang <jlhgln@gmail.com>
2016-02-24 00:02:24 +0800

    Fix typo

--- app/models/perturbation.rb
+++ app/models/perturbation.rb
@@ -1,5 +1,5 @@
 class Perturbation < ActiveRecord::Base
   validates_uniqueness_of :name
   has_many :bioassay_perturbation_count
-  has_many :bioassays, though: :bioassay_perturbation_count
+  has_many :bioassays, through: :bioassay_perturbation_count
 end
